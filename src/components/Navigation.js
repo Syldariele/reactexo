d1031934b205bc1b4602c59167655d3c441c441d
Sitael <beru666@live.fr>
2020-04-06 09:32:12 +0200

    add NavLink to color blue on Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
+import {NavLink} from 'react-router-dom';
 
 const Navigation = () => {
     return (
-        <Nav>
+        <Nav className="justify-content-end" variant="pills" >
             <Nav.Item>
-                <Nav.Link as={Link} to="/">
+                <Nav.Link as={NavLink} to="/" exact>
                     Accueil
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link as={Link} to="/comments/create">
+                <Nav.Link as={NavLink} to="/comments/create" >
                     Créer un commentaire
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link as={Link} to="/comments/delete">
+                <Nav.Link as={NavLink} to="/comments/delete" >
                     Supprimer un commentaire
                 </Nav.Link>
             </Nav.Item>
